fe4a9e2838b27ce2a4e9bc92a27ca603d296a598
Haule98dz <Haule98dz@gmail.com>
2021-08-05 04:50:31 +0700

    final

--- Mock1/src/menu.c
+++ Mock1/src/menu.c
@@ -3,9 +3,9 @@
  *
  */
 
-/*******************************************************************************
-* Includes
-******************************************************************************/
+ /*******************************************************************************
+  * Includes
+  ******************************************************************************/
 
 #include <stdint.h>
 #include <conio.h>
@@ -16,15 +16,16 @@
 #include "fatfs.h"
 #include "menu.h"
 
-/*******************************************************************************
-* Define
-******************************************************************************/
+  /*******************************************************************************
+   * Define
+   ******************************************************************************/
 
 typedef enum
 {
     menu_exit = 0,
-    menu_context_directory = 1,
-    menu_context_file = 2
+    menu_context_initiate_fat = 1,
+    menu_context_directory = 2,
+    menu_context_file = 3
 } menu_context_t;
 
 typedef enum
@@ -38,26 +39,36 @@ typedef enum
 * Variable
 ******************************************************************************/
 
-static menu_context_t s_menu_context = menu_context_directory;
+static menu_context_t s_menu_context = menu_context_initiate_fat;
+static fatfs_entry_struct_t* sp_dir_list = NULL;
+static uint8_t* sp_file_buffer = NULL;
+static uint32_t s_buffer_size = 0;
 
 /*******************************************************************************
 * Prototypes
 ******************************************************************************/
 
 /**
- * @brief Print list of entry
+ * @brief Get FAT image file path from keyboard, then initiate FAT
  *
- * @param entry_list Head of the list
+ * @return menu_error_code_t Error code
  */
-static menu_context_t menu_show_directory();
+static menu_error_code_t menu_initiate_fat();
 
 /**
- * @brief Print file content in hexa format
+ * @brief Print directory, then get user's input
  *
- * @param buff Buffer data
- * @param size Size of buffer data
+ * @return menu_error_code_t Error code
  */
-static menu_context_t menu_show_file(uint8_t *buff, uint32_t size);
+static menu_error_code_t menu_show_directory();
+
+/**
+ * @brief Print file content in hexa format, then get user's input
+ *
+ *
+ * @return menu_error_code_t Error code
+ */
+static menu_error_code_t menu_show_file();
 
 /**
  * @brief Print error
@@ -70,118 +81,161 @@ static void menu_print_error(fatfs_error_code_t error_code);
 * Static functions
 ******************************************************************************/
 
+static menu_error_code_t menu_initiate_fat()
+{
+    menu_error_code_t error_code = menu_error_code_get_file_name_failed;
+    int8_t file_path[261];
+
+    system("cls");
+    printf("Enter file path: ");
+    /* Get file name */
+    fflush(stdin);
+    if (0 != scanf("%[^\n]", &file_path[0]))
+    {
+        /* Init FAT */
+        error_code = fatfs_init(&file_path[0]);
+    }
+
+    if (error_code_success == error_code)
+    {
+        /* Read root directory */
+        error_code = fatfs_read_dir(0, &sp_dir_list);
+        if (error_code_success == error_code)
+        {
+            s_menu_context = menu_context_directory;
+        }
+    }
+
+    if (error_code_success != error_code)
+    {
+        s_menu_context = menu_exit;
+        menu_print_error(error_code);
+    }
+
+    return error_code;
+}
+
 static menu_error_code_t menu_show_directory()
 {
-    menu_error_code_t retVal;
-    static fatfs_entry_struct_t *sp_list_head = NULL;
-    fatfs_entry_struct_t *entry = sp_list_head;
+    menu_error_code_t error_code;
+    fatfs_entry_struct_t* entry = sp_dir_list;
     uint16_t index;
     uint32_t user_option;
 
-    /* Read directory list */
-    retVal = fatfs_read_dir((*selected_entry)->first_cluster, sp_list_head);
-
-    /* Print directory list */
+    /* Print directory */
     system("cls");
-    printf("Option   Name     Type     File size   Modified   \n");
+    printf(" Option       Name             Type           File size         Modified date and time\n");
+    printf("-----------------------------------------------------------------------------------------\n");
     for (index = 1; NULL != entry; index++)
     {
         printf("   %-5d   ", index);
-        printf("%-20s", entry->name);
+        printf("%-20s ", entry->name);
         if (0x2E != entry->name[0])
         {
             if (entry->is_subdir)
             {
-                printf("%-10s", "Folder");
+                printf("%-10s             ", "Folder");
             }
             else
             {
                 printf("%-10s", entry->extension);
-                printf("%-8d bytes", entry->size);
+                printf("%7d bytes", entry->size);
             }
-            printf("    %02d/%02d/%04d %02d:%02d\n", entry->modified_time.day, entry->modified_time.month, entry->modified_time.year, entry->modified_time.hour, entry->modified_time.minute);
+            printf("            %02d/%02d/%04d %02d:%02d", entry->modified_time.day, entry->modified_time.month, entry->modified_time.year, entry->modified_time.hour, entry->modified_time.minute);
         }
+        putchar('\n');
         entry = entry->next;
     }
 
-    /* Get user's option */
-    printf("\nEnter an option (enter 0 to exit): ");
-    retVal = menu_error_code_invalid_option;
-    if (0 != scanf("%u", &user_option))
+    /* Process user's option */
+    error_code = menu_error_code_invalid_option;
+    while (menu_error_code_success != error_code)
     {
-        if (0 < user_option)
+        printf("\nEnter an option (enter 0 to exit): ");
+        fflush(stdin);
+        if (0 != scanf("%u%*c", &user_option))
         {
-            for (index = 1, entry = sp_list_head; index != user_option && NULL != entry; index++)
+            if (0 < user_option)
             {
-                entry = entry->next;
+                for (index = 1, entry = sp_dir_list; index != user_option && NULL != entry; index++)
+                {
+                    entry = entry->next;
+                }
+                if (NULL != entry)
+                {
+                    error_code = menu_error_code_success;
+                    if (entry->is_subdir)
+                    {
+                        error_code = fatfs_read_dir(entry->first_cluster, &sp_dir_list);
+                        s_menu_context = menu_context_directory;
+                    }
+                    else
+                    {
+                        s_buffer_size = entry->size;
+                        sp_file_buffer = (uint8_t*)malloc(entry->rounded_size);
+                        error_code = fatfs_read_file(entry->first_cluster, &sp_file_buffer, entry->rounded_size);
+                        s_menu_context = menu_context_file;
+                    }
+                }
             }
-            if (NULL != entry)
+            else
             {
-                retVal = menu_error_code_success;
-                *selected_entry = entry;
-                if (entry->is_subdir)
-                {
-                    s_menu_context = menu_context_directory;
-                }
-                else
-                {
-                    s_menu_context = menu_context_file;
-                }
+                error_code = menu_error_code_success;
+                s_menu_context = menu_context_initiate_fat;
             }
         }
-        else
+        if (error_code_success != error_code)
         {
-            retVal = menu_error_code_success;
-            s_menu_context = menu_exit;
+            menu_print_error(error_code);
         }
     }
 
-    return retVal;
+    return error_code;
 }
 
-static menu_error_code_t menu_show_file(fatfs_entry_struct_t *selected_entry)
+static menu_error_code_t menu_show_file()
 {
-    menu_error_code_t retVal;
+    menu_error_code_t error_code = menu_error_code_invalid_option;
     uint32_t index;
-    static uint8_t *sp_file_buffer = NULL;
+    uint32_t user_option;
 
-
-    /* Read file */
-    retVal = fatfs_read_file(selected_entry->first_cluster, &sp_file_buffer, selected_entry->rounded_size);
-
-    if (error_code_success == retVal)
+    /* Print file */
+    system("cls");
+    for (index = 0; index < s_buffer_size; index++)
     {
-        /* Print file */
-        system("cls");
-        for (index = 0; index < selected_entry->rounded_size; index++)
-        {
-            printf("%x", sp_file_buffer[index]);
-        }
+        printf("%x", sp_file_buffer[index]);
+    }
+    free(sp_file_buffer);
 
-        /* Get user's option */
+    printf("\n\nDone printing %u bytes.", s_buffer_size);
+    /* Process user's option */
+    while (menu_error_code_success != error_code)
+    {
         printf("\nEnter 1 to return to subdirectory, enter 0 to exit: ");
-        if (0 != scanf("%u", &user_option))
+        fflush(stdin);
+        if (0 != scanf("%u%*c", &user_option))
         {
             switch (user_option)
             {
                 case 0:
-                    s_menu_context = menu_exit;
+                    error_code = menu_error_code_success;
+                    s_menu_context = menu_context_initiate_fat;
                     break;
                 case 1:
+                    error_code = menu_error_code_success;
                     s_menu_context = menu_context_directory;
                     break;
                 default:
-                    retVal = menu_error_code_invalid_option;
                     break;
             }
         }
-        else
+        if (error_code_success != error_code)
         {
-            retVal = menu_error_code_invalid_option;
+            menu_print_error(error_code);
         }
     }
 
-    return retVal;
+    return error_code;
 }
 
 static void menu_print_error(fatfs_error_code_t error_code)
@@ -218,50 +272,27 @@ static void menu_print_error(fatfs_error_code_t error_code)
 void menu(void)
 {
     fatfs_error_code_t error_code;
-    int8_t file_path[261];
-    fatfs_entry_struct_t *user_selection = NULL;
     bool stop = false;
 
-    printf("Enter file path: ");
-    /* Get file name */
-    if (0 != scanf("%[^\n]", &file_path[0]))
+    while (!stop)
     {
-        /* Init FAT */
-        error_code = fatfs_init(&file_path[0]);
-        if (error_code_success == error_code)
+        switch (s_menu_context)
         {
-            /* Read root directory */
-            error_code = fatfs_read_dir(&user_option);
+            case menu_context_initiate_fat:
+                error_code = menu_initiate_fat();
+                break;
+            case menu_context_directory:
+                error_code = menu_show_directory();
+                break;
+            case menu_context_file:
+                error_code = menu_show_file();
+                break;
+            case menu_exit:
+                stop = true;
+                break;
         }
     }
-    else
-    {
-        error_code = menu_error_code_get_file_name_failed;
-    }
-
-    if (error_code_success == error_code)
-    {
-        while (!stop)
-        {
-            switch (s_menu_context)
-            {
-                case menu_context_directory:
-                    error_code = menu_show_directory(&user_selection);
-                    break;
-                case menu_context_file:
-                    error_code = menu_show_file(user_selection);
-                    break;
-                case menu_exit:
-                    stop = true;
-                    break;
-            }
-        }
-    }
-
-    if (error_code_success == error_code)
-    {
-        error_code = fatfs_deinit();
-    }
+    error_code = fatfs_deinit();
 
     if (error_code_success != error_code)
     {
